373e7697f0dc0a21dcd34e3d3129d0ac057bd893
KumonShota <b17u0059@outlook.jp>
2025-02-10 05:18:37 +0000

    model controller added

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    public function index(Review $post)
+    {
+        return $post->get();
+    }
 }
